a279dee264493bc5b09716fd984c3d549dbef94b
Anastasiia Perova <perovaa.24@gmail.com>
2023-03-22 21:52:32 +0400

    feat: add animation to logo

--- src/components/recipeCardList/RecepiCardList.tsx
+++ src/components/recipeCardList/RecepiCardList.tsx
@@ -3,7 +3,7 @@ import { RecipeCard } from '../recipeCard/recipeCard';
 import { RECIPE_MOCK_DATA } from './recipeMockData';
 
 export function RecipeCardList() {
-  const cards = [1, 2, 3, 4, 5];
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8];
   const data = RECIPE_MOCK_DATA;
   return (
     <Row gutter={[16, { xs: 8, sm: 16, md: 24, lg: 32 }]} justify="start">
