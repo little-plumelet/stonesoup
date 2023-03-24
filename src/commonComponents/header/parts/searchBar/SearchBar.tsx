de02347acb6937efd6c21d08472a1db207dc8c0f
Anastasiia Perova <perovaa.24@gmail.com>
2023-03-24 18:29:40 +0400

    feat: add axios & create get request (search recipe)

--- src/commonComponents/header/parts/searchBar/SearchBar.tsx
+++ src/commonComponents/header/parts/searchBar/SearchBar.tsx
@@ -1,10 +1,30 @@
-import { Input } from 'antd';
+import { Input, message } from 'antd';
+import axios from 'axios';
 
 const { Search } = Input;
+const API_KEY = import.meta.env.VITE_SPINACULAR_API_KEY;
 
-const searchHandle = (value: string) => {
-  console.log(value);
-};
+async function searchHandle(value: string) {
+  axios
+    .get('https://api.spoonacular.com/recipes/complexSearch', {
+      params: {
+        query: value,
+        addRecipeInformation: true,
+        apiKey: API_KEY,
+        number: 8,
+      },
+    })
+    .then((response) => {
+      console.log('data = ', response?.data?.results);
+    })
+    .catch((error) => {
+      if (axios.isAxiosError(error)) {
+        message.error(error.message);
+      } else {
+        message.error('An unexpected error occurred');
+      }
+    });
+}
 
 export function SearchBar() {
   return (
