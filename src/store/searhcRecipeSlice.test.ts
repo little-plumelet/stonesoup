4a9e8675f2364a489e220162ece459b1ce8b6d26
Anastasiia Perova <perovaa.24@gmail.com>
2023-03-31 19:26:28 +0400

    feat: add tests for searchRecipeSlice

--- src/store/searhcRecipeSlice.test.ts
+++ src/store/searhcRecipeSlice.test.ts
@@ -3,6 +3,7 @@ import searchedRecipeReducer, {
   addSearchValue,
   ISearchedRecipesState,
   searchRecipes,
+  getMoreRecipes,
 } from './searchRecipe.slice';
 
 const initialState: ISearchedRecipesState = {
@@ -12,6 +13,19 @@ const initialState: ISearchedRecipesState = {
   offset: 0,
   totalResults: 0,
   error: null,
+};
+
+const mockData = {
+  id: 716429,
+  title: 'Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs',
+  image: 'https://spoonacular.com/recipeImages/716429-556x370.jpg',
+  readyInMinutes: 45,
+  aggregateLikes: 209,
+};
+
+const searchRecipesMockProps = {
+  value: '',
+  offset: 0,
 };
 
 jest.mock('axios');
@@ -34,27 +48,11 @@ test('addSearchValue', () => {
 });
 
 describe('get recipes via search', () => {
-  it('get recipes with success', async () => {
-    const mockData = {
-      id: 716429,
-      title: 'Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs',
-      image: 'https://spoonacular.com/recipeImages/716429-556x370.jpg',
-      readyInMinutes: 45,
-      aggregateLikes: 209,
-    };
-
+  it('get recipes with success thunk', async () => {
     (axios.get as jest.Mock).mockImplementation(() =>
       Promise.resolve({
         data: {
-          results: [
-            {
-              id: 716429,
-              title: 'Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs',
-              image: 'https://spoonacular.com/recipeImages/716429-556x370.jpg',
-              readyInMinutes: 45,
-              aggregateLikes: 209,
-            },
-          ],
+          results: [{ ...mockData }],
           offset: 46,
           totalResults: 59,
         },
@@ -62,26 +60,203 @@ describe('get recipes via search', () => {
     );
 
     const dispatch = jest.fn();
-    const thunk = searchRecipes({ value: '', offset: 0 });
+    const thunk = searchRecipes(searchRecipesMockProps);
 
     await thunk(dispatch, () => ({}), {});
 
     const { calls } = dispatch.mock;
-
-    expect(calls).toHaveLength(2);
-
     const [start, end] = calls;
 
+    expect(calls).toHaveLength(2);
     expect(start[0].type).toEqual(
-      'searchedRecipesStore/searchRecepies/pending'
+      searchRecipes.pending('', searchRecipesMockProps).type
     );
-    expect(end[0].type).toEqual(
-      'searchedRecipesStore/searchRecepies/fulfilled'
-    );
+    expect(end[0].type).toEqual('searchedRecipesStore/searchRecipes/fulfilled');
     expect(end[0].payload).toEqual({
       recipes: [{ ...mockData }],
       offset: 46,
       totalResults: 59,
     });
+  });
+
+  it('get recipes with error thunk', async () => {
+    (axios.get as jest.Mock).mockImplementation(() =>
+      Promise.reject(new Error())
+    );
+
+    const dispatch = jest.fn();
+    const thunk = searchRecipes(searchRecipesMockProps);
+
+    await thunk(dispatch, () => ({}), {});
+
+    const { calls } = dispatch.mock;
+    const [start, end] = calls;
+
+    expect(calls).toHaveLength(2);
+    expect(start[0].type).toEqual(
+      searchRecipes.pending('', searchRecipesMockProps).type
+    );
+    expect(end[0].type).toEqual(
+      searchRecipes.rejected(null, '', searchRecipesMockProps).type
+    );
+    expect(end[0].payload).toEqual('Unknown error occurred');
+    expect(end[0].meta.rejectedWithValue).toEqual(true);
+  });
+
+  // extra-reducers
+  it('search recipes pending', () => {
+    const state = searchedRecipeReducer(
+      initialState,
+      searchRecipes.pending('', searchRecipesMockProps)
+    );
+
+    expect(state.loading).toBe(true);
+    expect(state.error).toBeNull();
+  });
+
+  it('search recipes fulfilled', () => {
+    const expectedState = {
+      totalResults: 1,
+      recipes: [{ ...mockData }],
+      offset: 0,
+    };
+    const state = searchedRecipeReducer(
+      initialState,
+      searchRecipes.fulfilled({ ...expectedState }, '', {
+        value: '',
+        offset: 0,
+      })
+    );
+    expect(state).toEqual({
+      loading: false,
+      error: null,
+      offset: expectedState.recipes.length,
+      list: expectedState.recipes,
+      totalResults: expectedState.totalResults,
+      searchValue: '',
+    });
+  });
+
+  it('search recipes rejected', () => {
+    const state = searchedRecipeReducer(
+      initialState,
+      searchRecipes.rejected(
+        new Error(),
+        '',
+        searchRecipesMockProps,
+        'Unknown error occured'
+      )
+    );
+
+    expect(state).toEqual({
+      ...initialState,
+      error: 'Unknown error occured',
+      loading: false,
+    });
+  });
+});
+
+describe('get more recipes', () => {
+  it('get more recipes with succses', async () => {
+    (axios.get as jest.Mock).mockImplementation(() =>
+      Promise.resolve({
+        data: {
+          results: [{ ...mockData }],
+          offset: 21,
+          totalResults: 22,
+        },
+      })
+    );
+
+    const dispatch = jest.fn();
+    const thunk = getMoreRecipes({ value: '', offset: 8 });
+
+    await thunk(dispatch, () => {}, {});
+
+    const { calls } = dispatch.mock;
+    const [start, end] = calls;
+
+    expect(calls).toHaveLength(2);
+    expect(start[0].type).toEqual(
+      getMoreRecipes.pending('', { value: '', offset: 0 }).type
+    );
+    expect(end[0].type).toEqual(
+      'searchedRecipesStore/getMoreRecipes/fulfilled'
+    );
+    expect(end[0].payload).toEqual({
+      recipes: [{ ...mockData }],
+      offset: 21,
+      totalResults: 22,
+    });
+  });
+
+  it('get more recipes with error', async () => {
+    (axios.get as jest.Mock).mockImplementation(() =>
+      Promise.reject(new Error())
+    );
+
+    const dispatch = jest.fn();
+    const thunk = getMoreRecipes({ value: '', offset: 8 });
+
+    await thunk(dispatch, () => {}, {});
+
+    const { calls } = dispatch.mock;
+    const [start, end] = calls;
+
+    expect(calls).toHaveLength(2);
+    expect(start[0].type).toEqual(
+      getMoreRecipes.pending('', { value: '', offset: 0 }).type
+    );
+    expect(end[0].type).toEqual('searchedRecipesStore/getMoreRecipes/rejected');
+    expect(end[0].payload).toEqual('Unknown error occurred');
+    expect(end[0].meta.rejectedWithValue).toBe(true);
+  });
+
+  // extra reducers
+  it('get more recipes pending', () => {
+    const state = searchedRecipeReducer(
+      initialState,
+      getMoreRecipes.pending('', searchRecipesMockProps)
+    );
+
+    expect(state.error).toBeNull();
+    expect(state.loading).toBeTruthy();
+  });
+
+  it('get more recipes fulfilled', () => {
+    const expectedState = {
+      totalResults: 1,
+      recipes: [{ ...mockData }],
+      offset: 0,
+    };
+
+    const state = searchedRecipeReducer(
+      initialState,
+      getMoreRecipes.fulfilled({ ...expectedState }, '', searchRecipesMockProps)
+    );
+
+    expect(state).toEqual({
+      error: null,
+      loading: false,
+      list: [...expectedState.recipes],
+      offset: expectedState.recipes.length,
+      totalResults: expectedState.totalResults,
+      searchValue: '',
+    });
+  });
+
+  it('get more recipes rejected', () => {
+    const state = searchedRecipeReducer(
+      initialState,
+      getMoreRecipes.rejected(
+        new Error(),
+        '',
+        searchRecipesMockProps,
+        'Unknown error occured'
+      )
+    );
+
+    expect(state.error).toEqual('Unknown error occured');
+    expect(state.loading).toBeFalsy();
   });
 });
