2d33e947ebd1ecdb2ec0ce3cc4c07a1ddd660ed5
Anastasiia Perova <perovaa.24@gmail.com>
2023-04-04 11:50:14 +0400

    feat: add tests for Navbar

---
+++ src/commonComponents/header/parts/navBar/navBar.test.tsx
@@ -0,0 +1,31 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { NavBar } from './NavBar';
+
+describe('navbar', () => {
+  it('should be rendered', () => {
+    const component = render(
+      <MemoryRouter>
+        <NavBar />
+      </MemoryRouter>
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+  it('links work', () => {
+    const history = createMemoryHistory();
+
+    render(
+      <Router location={history.location} navigator={history}>
+        <NavBar />
+      </Router>
+    );
+
+    fireEvent.click(screen.getByText('home'));
+    expect(history.location.pathname).toBe('/');
+
+    fireEvent.click(screen.getByText('about us'));
+    expect(history.location.pathname).toBe('/about');
+  });
+});
